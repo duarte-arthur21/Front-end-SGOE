c735ba197b6406bb2f758f44e0fb4806d4e08440
duarte-arthur21 <73848248+duarte-arthur21@users.noreply.github.com>
2021-07-05 21:52:56 -0300

    commit teste

---
+++ script/scripit.js
@@ -0,0 +1,12 @@
+const desc = document.querySelector("#descricao");
+
+console.log(desc);
+
+desc.addEventListener("keypress", function(e){
+    const inputLength = desc.value.length;
+    const maxChars = 0;
+
+    if(inputLength >= maxChars){
+        e.preventDefault();
+    }
+});
